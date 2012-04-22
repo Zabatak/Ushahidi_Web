695f62269f59566cc0291b5e8dcc3991f1a1be4e
Abbas Ibrahim <abbas.adel@mufix.org>
2012-04-22 20:43:03 +0200

    set log level debug

--- application/config/config.php
+++ application/config/config.php
@@ -4,7 +4,7 @@
  * then a full URL will be used, eg: http://localhost/ushahidi/. If it only includes
  * the path, and a site_protocol is specified, the domain will be auto-detected.
  */
-$config['site_domain'] = '/';
+$config['site_domain'] = '/zabatak';
 
 /**
  * Force a default protocol to be used by the site. If no site_protocol is
@@ -81,7 +81,7 @@ $config['enable_hooks'] = TRUE;
  *  3 - Notices
  *  4 - Debugging
  */
-$config['log_threshold'] = 1;
+$config['log_threshold'] = 4;
 
 /**
  * Message logging directory.
